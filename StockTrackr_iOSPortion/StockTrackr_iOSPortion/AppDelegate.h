6286fda66845c9a504542406e9d6bcba13a00370
Janelle Salisbury <janellesalisbury@fullsail.edu>
2013-09-10 16:35:43 -0400

    StockTrackr_iOS

---
+++ StockTrackr_iOSPortion/StockTrackr_iOSPortion/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  StockTrackr_iOSPortion
+//
+//  Created by Janelle Salisbury on 9/10/13.
+//  Copyright (c) 2013 Janelle Salisbury. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ StockTrackr_iOSPortion/StockTrackr_iOSPortion/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  StockTrackr_iOSPortion
+//
+//  Created by Janelle Salisbury on 9/10/13.
+//  Copyright (c) 2013 Janelle Salisbury. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
