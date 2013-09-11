bc9097a661e121a7be6bb398f78d6d87ec55b10f
Janelle Salisbury <janellesalisbury@fullsail.edu>
2013-09-11 18:27:23 -0400

    Android portion complete!

--- StockTrackr/src/com/stocktrackr/MainActivity.java
+++ StockTrackr/src/com/stocktrackr/MainActivity.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import com.stocktrackr.db.StocksDataSource;
 import com.stocktrackr.model.Stock;
+import com.stocktrackr.xml.StocksPullParser;
+
 import android.os.Bundle;
 import android.app.ListActivity;
 import android.util.Log;
@@ -22,9 +24,7 @@ public class MainActivity extends ListActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		//StocksPullParser parser = new StocksPullParser();
-		//List<Stock> stocks = parser.parseXML(this);
-		
+	
 		datasource = new StocksDataSource(this);
 		datasource.open();
 		
@@ -58,51 +58,13 @@ public class MainActivity extends ListActivity {
 	}
 
 	private void createData(){
-		Stock stock = new Stock();
-		stock.setName("Apple Inc");
-		stock.setSymbol("AAPL");
-		stock.setLastPrice(487.115);
-		stock.setChange(-0.100999999999999);
-		stock.setVolume(568074);
-		stock = datasource.create(stock);
-		Log.i(LOGTAG, "Stock created with id " + stock.getId());
+		StocksPullParser parser = new StocksPullParser();
+		List<Stock> stocks = parser.parseXML(this);
 		
-	    stock = new Stock();
-		stock.setName("Bank of America Corp");
-		stock.setSymbol("BAC");
-		stock.setLastPrice(14.115);
-		stock.setChange(-0.00499999999999901);
-		stock.setVolume(4378773);
-		stock = datasource.create(stock);
-		Log.i(LOGTAG, "Stock created with id " + stock.getId());
+		for(Stock stock : stocks){
+			datasource.create(stock);
+		}
 		
-		stock = new Stock();
-		stock.setName("Facebook, Inc");
-		stock.setSymbol("FB");
-		stock.setLastPrice(41.31);
-		stock.setChange(0.0160000000000053);
-		stock.setVolume(3425912);
-		stock = datasource.create(stock);
-		Log.i(LOGTAG, "Stock created with id " + stock.getId());
-		
-		stock = new Stock();
-		stock.setName("General Motors Co");
-		stock.setSymbol("GM");
-		stock.setLastPrice(23.1);
-		stock.setChange(-0.0399999999999991);
-		stock.setVolume(2786454);
-		stock = datasource.create(stock);
-		Log.i(LOGTAG, "Stock created with id " + stock.getId());
-		
-		stock = new Stock();
-		stock.setName("Sirius SM Radio Inc");
-		stock.setSymbol("SIRI");
-		stock.setLastPrice(3.58);
-		stock.setChange(-0.0499999999999998);
-		stock.setVolume(1816606);
-		stock = datasource.create(stock);
-		Log.i(LOGTAG, "Stock created with id " + stock.getId());
-	
 	}
 
 }
