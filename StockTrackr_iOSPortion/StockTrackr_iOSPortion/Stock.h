aa27cb224ba23c33391328fabecc31749a7d7c15
Janelle Salisbury <janellesalisbury@fullsail.edu>
2013-09-11 19:47:27 -0400

    Adding proper file extensions

---
+++ StockTrackr_iOSPortion/StockTrackr_iOSPortion/Stock.h
@@ -0,0 +1,13 @@
+//
+//  Stock.h
+//  StockTrackr_iOSPortion
+//
+//  Created by Janelle Salisbury on 9/11/13.
+//  Copyright (c) 2013 Janelle Salisbury. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Stock : UIViewController
+
+@end
---
+++ StockTrackr_iOSPortion/StockTrackr_iOSPortion/Stock.m
@@ -0,0 +1,38 @@
+//
+//  Stock.m
+//  StockTrackr_iOSPortion
+//
+//  Created by Janelle Salisbury on 9/11/13.
+//  Copyright (c) 2013 Janelle Salisbury. All rights reserved.
+//
+
+#import "Stock.h"
+
+@interface Stock ()
+
+@end
+
+@implementation Stock
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
